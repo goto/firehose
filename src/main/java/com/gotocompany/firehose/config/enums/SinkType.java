24e807cd7aed7e6f128d38b4f4c8e8269ff284af
Sumit Aich <aich.1998@gmail.com>
2023-06-19 17:34:09 +0530

    fix: deprecate HTTP SinkType

--- src/main/java/com/gotocompany/firehose/config/enums/SinkType.java
+++ src/main/java/com/gotocompany/firehose/config/enums/SinkType.java
@@ -2,6 +2,11 @@ package com.gotocompany.firehose.config.enums;
 
 public enum SinkType {
     JDBC,
+    /**
+     * @deprecated
+     * HTTP is deprecated. Please consider using HTTPV2 instead.
+     */
+    @Deprecated
     HTTP,
     HTTPV2,
     LOG,
