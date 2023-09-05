47b56d4f7bc1935770ddd51434122876922a439a
sushmith <bsushmith@live.com>
2023-09-05 15:42:26 +0530

    fix: uri parser for json input

--- src/main/java/com/gotocompany/firehose/sink/http/request/uri/UriParser.java
+++ src/main/java/com/gotocompany/firehose/sink/http/request/uri/UriParser.java
@@ -1,6 +1,9 @@
 package com.gotocompany.firehose.sink.http.request.uri;
 
 
+import com.google.gson.JsonObject;
+import com.gotocompany.firehose.config.enums.InputSchemaType;
+import com.gotocompany.firehose.exception.JsonParseException;
 import com.gotocompany.firehose.message.Message;
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.DynamicMessage;
@@ -8,7 +11,11 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import com.gotocompany.stencil.Parser;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.common.errors.InvalidConfigurationException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,15 +25,38 @@ import java.util.List;
 public class UriParser {
     private Parser protoParser;
     private String parserMode;
+    private JSONParser jsonParser;
+
 
     public UriParser(Parser protoParser, String parserMode) {
         this.protoParser = protoParser;
         this.parserMode = parserMode;
+        this.jsonParser = new JSONParser();
+    }
+
+    public UriParser(Parser protoParser, String parserMode, JSONParser jsonParser) {
+        this.protoParser = protoParser;
+        this.parserMode = parserMode;
+        this.jsonParser = jsonParser;
     }
 
     public String parse(Message message, String serviceUrl) {
-        DynamicMessage parsedMessage = parseEsbMessage(message);
-        return parseServiceUrl(parsedMessage, serviceUrl);
+
+//        if (message.getInputSchemaType() == InputSchemaType.JSON) {
+//            JSONObject jsonObject = parseJsonMessage(message);
+//
+//        }
+//        create jonobject if input is json
+//        {
+//        }
+//        DynamicMessage parsedMessage = parseEsbMessage(message);
+
+//        -- > mpve this to directlydo it inside parseServiceURl
+
+
+//        take call inside parseService Url
+//        overload parseServiceUrl function to take jsonObject
+        return parseServiceUrl(message, serviceUrl);
 
     }
 
@@ -40,7 +70,18 @@ public class UriParser {
         return parsedMessage;
     }
 
-    private String parseServiceUrl(DynamicMessage data, String serviceUrl) {
+    private JSONObject parseJsonMessage(Message message) {
+        JSONObject jsonObject;
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(new String(message.getLogMessage(), StandardCharsets.UTF_8));
+        } catch (ParseException e) {
+            throw new JsonParseException(e.getMessage(), e.getCause());
+        }
+        return jsonObject;
+    }
+
+
+    private String parseServiceUrl(Message message, String serviceUrl) {
         if (StringUtils.isEmpty(serviceUrl)) {
             throw new IllegalArgumentException("Service URL '" + serviceUrl + "' is invalid");
         }
@@ -55,16 +96,43 @@ public class UriParser {
 
         String urlPattern = urlStrings[0];
         String urlVariables = StringUtils.join(Arrays.copyOfRange(urlStrings, 1, urlStrings.length), ",");
-        String renderedUrl = renderStringUrl(data, urlPattern, urlVariables);
-        return StringUtils.isEmpty(urlVariables)
-                ? urlPattern
-                : renderedUrl;
+
+        if (StringUtils.isEmpty(urlVariables)) {
+            return urlPattern;
+        }
+
+        String renderedUrl;
+        if (message.getInputSchemaType() == InputSchemaType.JSON) {
+            JSONObject json = parseJsonMessage(message);
+            renderedUrl = renderStringUrl(json, urlPattern, urlVariables);
+        } else {
+            // InputSchemaType.PROTOBUF
+            DynamicMessage data = parseEsbMessage(message);
+            renderedUrl = renderStringUrl(data, urlPattern, urlVariables);
+        }
+
+        return renderedUrl;
     }
 
-    private String renderStringUrl(DynamicMessage parsedMessage, String pattern, String patternVariables) {
-        if (StringUtils.isEmpty(patternVariables)) {
-            return pattern;
+    private String renderStringUrl(JSONObject jsonObject, String pattern, String patternVariables) {
+        List<String> patternVariablesList = Arrays.asList(patternVariables.split(","));
+        Object[] patternVariableData = patternVariablesList
+                .stream()
+                .map(field -> getDataByFieldName(jsonObject, field))
+                .toArray();
+        return String.format(pattern, patternVariableData);
+    }
+
+    private Object getDataByFieldName(JSONObject jsonObject, String fieldName) {
+        if (!jsonObject.containsKey(fieldName)) {
+            throw new IllegalArgumentException("Invalid json field name: " + fieldName);
         }
+
+        return jsonObject.get(fieldName);
+    }
+
+
+    private String renderStringUrl(DynamicMessage parsedMessage, String pattern, String patternVariables) {
         List<String> patternVariableFieldNumbers = Arrays.asList(patternVariables.split(","));
         Object[] patternVariableData = patternVariableFieldNumbers
                 .stream()
