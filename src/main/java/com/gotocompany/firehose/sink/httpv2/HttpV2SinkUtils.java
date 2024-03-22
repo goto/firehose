8509023f59adc9e2587e1faf8e887d41b9f75b51
Sumit Aich <aich.1998@gmail.com>
2024-03-22 19:32:44 +0530

    fix: handle null values for kafka consumer mode and sink pool num threads

--- src/main/java/com/gotocompany/firehose/sink/httpv2/HttpV2SinkUtils.java
+++ src/main/java/com/gotocompany/firehose/sink/httpv2/HttpV2SinkUtils.java
@@ -1,15 +1,19 @@
 package com.gotocompany.firehose.sink.httpv2;
 
+import com.gotocompany.firehose.config.enums.KafkaConsumerMode;
+
 import java.util.Map;
 
 public class HttpV2SinkUtils {
-    private static final String SYNC_MODE = "sync";
 
     public static void addAdditionalConfigsForHttpV2Sink(Map<String, String> env) {
-        if (SYNC_MODE.equalsIgnoreCase(env.get("SOURCE_KAFKA_CONSUMER_MODE"))) {
-            env.put("SINK_HTTPV2_MAX_CONNECTIONS", "1");
-        } else {
-            env.put("SINK_HTTPV2_MAX_CONNECTIONS", env.get("SINK_POOL_NUM_THREADS"));
+
+        switch (KafkaConsumerMode.valueOf(env.getOrDefault("SOURCE_KAFKA_CONSUMER_MODE", "SYNC"))) {
+            case SYNC:
+                env.put("SINK_HTTPV2_MAX_CONNECTIONS", "1");
+                break;
+            case ASYNC:
+                env.put("SINK_HTTPV2_MAX_CONNECTIONS", env.getOrDefault("SINK_POOL_NUM_THREADS", "1"));
         }
     }
 }
