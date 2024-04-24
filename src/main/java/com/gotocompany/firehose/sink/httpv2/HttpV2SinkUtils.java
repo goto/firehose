fd55859fdc2d6053312fc50b0384ce3a367eda70
Sumit Aich <aich.1998@gmail.com>
2024-04-25 04:15:16 +0530

    fix: fix checkstyle

--- src/main/java/com/gotocompany/firehose/sink/httpv2/HttpV2SinkUtils.java
+++ src/main/java/com/gotocompany/firehose/sink/httpv2/HttpV2SinkUtils.java
@@ -8,6 +8,7 @@ public class HttpV2SinkUtils {
 
     public static void addAdditionalConfigsForHttpV2Sink(Map<String, String> env) {
 
+        System.out.println(env.getOrDefault("SOURCE_KAFKA_CONSUMER_MODE", "SYNC").toUpperCase());
         switch (KafkaConsumerMode.valueOf(env.getOrDefault("SOURCE_KAFKA_CONSUMER_MODE", "SYNC").toUpperCase())) {
             case SYNC:
                 env.put("SINK_HTTPV2_MAX_CONNECTIONS", "1");
@@ -15,7 +16,7 @@ public class HttpV2SinkUtils {
 
             case ASYNC:
                 env.put("SINK_HTTPV2_MAX_CONNECTIONS", env.getOrDefault("SINK_POOL_NUM_THREADS", "1"));
-
+                break;
             default:
                 throw new IllegalArgumentException("Consumer mode should be async or sync");
 
