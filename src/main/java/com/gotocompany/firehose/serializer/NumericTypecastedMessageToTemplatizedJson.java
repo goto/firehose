7688ecf04f11015ed21b3a97fc377998531eb3e4
Eka Winata <eka.winata@gopay.co.id>
2024-06-13 14:35:45 +0700

    directly use map function

--- src/main/java/com/gotocompany/firehose/serializer/NumericTypecastedMessageToTemplatizedJson.java
+++ src/main/java/com/gotocompany/firehose/serializer/NumericTypecastedMessageToTemplatizedJson.java
@@ -6,7 +6,6 @@ import com.gotocompany.firehose.message.Message;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 
-import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -27,33 +26,8 @@ public class NumericTypecastedMessageToTemplatizedJson implements MessageSeriali
         DocumentContext documentContext = JsonPath.parse(templatizedOutput);
 
         for (Map.Entry<String, Function<String, Number>> entry : serializerConfig.serializerJsonNumericTypecast().entrySet()) {
-            replaceSingleJsonPath(documentContext, entry.getKey(), entry.getValue());
+            documentContext.map(entry.getKey(), (currentValue, configuration) -> entry.getValue().apply(currentValue.toString()));
         }
         return documentContext.jsonString();
-    }
-
-    private void replaceSingleJsonPath(DocumentContext documentContext,
-                                      String jsonPathExpression,
-                                      Function<String, Number> mapper) {
-        Object originalValues = documentContext.read(jsonPathExpression);
-
-        if (!(originalValues instanceof List)) {
-            typecastSingleField(jsonPathExpression, originalValues.toString(), mapper, documentContext);
-            return;
-        }
-        for (String originalValue : (List<String>) originalValues) {
-            typecastSingleField(jsonPathExpression, originalValue, mapper, documentContext);
-        }
-    }
-
-    private void typecastSingleField(String jsonPathExpression,
-                                     String originalValue,
-                                     Function<String, Number> mapper,
-                                     DocumentContext documentContext) {
-        String fieldName = jsonPathExpression.substring(jsonPathExpression.lastIndexOf(".") + 1);
-        String[] segment = jsonPathExpression.split("\\." + fieldName + "$");
-        String classifier = String.format("%s[?(@.%s == \"%s\")].%s", segment[0], fieldName, originalValue, fieldName);
-        JsonPath jsonPath = JsonPath.compile(classifier);
-        documentContext.set(jsonPath, mapper.apply(originalValue));
     }
 }
