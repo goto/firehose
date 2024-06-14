76842844350def5f03283989189f038aeee4238b
Eka Winata <eka.winata@gopay.co.id>
2024-06-14 16:01:44 +0700

    Add comment

--- src/main/java/com/gotocompany/firehose/serializer/TypecastedJsonSerializer.java
+++ src/main/java/com/gotocompany/firehose/serializer/TypecastedJsonSerializer.java
@@ -11,18 +11,36 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.Map;
 import java.util.function.Function;
 
+/***
+ * MessageSerializer wrapping other JSON MessageSerializer which add capability to typecast
+ * some of the fields from the inner serializer
+ */
 @Slf4j
 public class TypecastedJsonSerializer implements MessageSerializer {
 
     private final MessageSerializer messageSerializer;
     private final SerializerConfig serializerConfig;
 
+    /**
+     * Constructor for TypecastedJsonSerializer.
+     *
+     * @param messageSerializer the inner serializer to be wrapped
+     * @param serializerConfig the configuration for typecasting, where each map contains
+     *                       a JSON path and the desired type
+     */
     public TypecastedJsonSerializer(MessageSerializer messageSerializer,
                                     SerializerConfig serializerConfig) {
         this.messageSerializer = messageSerializer;
         this.serializerConfig = serializerConfig;
     }
 
+    /**
+     * Serializes the given message, then applies typecasting to specified fields in the resulting JSON.
+     *
+     * @param message the message to be serialized
+     * @return the serialized and typecasted JSON string
+     * @throws DeserializerException if an error occurs during serialization or typecasting
+     */
     @Override
     public String serialize(Message message) throws DeserializerException {
         String jsonString = messageSerializer.serialize(message);
