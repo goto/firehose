5b538b38e03e1708f6751a3e9755e6602d2395fb
Eka Winata <eka.winata@gopay.co.id>
2024-07-10 17:26:12 +0700

    Add assertions for buildGrpcMetadata

--- src/test/java/com/gotocompany/firehose/proto/ProtoToMetadataMapperTest.java
+++ src/test/java/com/gotocompany/firehose/proto/ProtoToMetadataMapperTest.java
@@ -4,6 +4,7 @@ import com.gotocompany.firehose.consumer.GenericError;
 import com.gotocompany.firehose.consumer.GenericResponse;
 import io.grpc.Metadata;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -12,12 +13,10 @@ import java.util.Map;
 public class ProtoToMetadataMapperTest {
 
     @Test
-    public void test() throws IOException {
+    public void buildGrpcMetadataShouldBuildDynamicMetadataWithCorrectPlaceholders() throws IOException {
         Map<String, Object> template = new HashMap<>();
         template.put("$GenericResponse.detail", "$GenericResponse.success");
         template.put("staticKey", "$(GenericResponse.errors[0].cause + GenericResponse.errors[0].code)");
-        template.put("shouldBeBoolean", true);
-        template.put("staticInt", 12345);
         ProtoToMetadataMapper protoToMetadataMapper = new ProtoToMetadataMapper(
                 GenericResponse.getDescriptor(),
                 template
@@ -27,11 +26,16 @@ public class ProtoToMetadataMapperTest {
                 .setDetail("detail_of_error")
                 .addErrors(GenericError.newBuilder()
                         .setCode("404")
-                        .setCause("Not Found")
+                        .setCause("not_found")
                         .setEntity("GTF")
                         .build())
                 .build();
+
         Metadata metadata = protoToMetadataMapper.buildGrpcMetadata(payload);
-        System.out.println(metadata);
+
+        Assertions.assertTrue(metadata.containsKey(Metadata.Key.of("detail_of_error", Metadata.ASCII_STRING_MARSHALLER)));
+        Assertions.assertEquals("false", metadata.get(Metadata.Key.of("detail_of_error", Metadata.ASCII_STRING_MARSHALLER)));
+        Assertions.assertTrue(metadata.containsKey(Metadata.Key.of("statickey", Metadata.ASCII_STRING_MARSHALLER)));
+        Assertions.assertEquals("not_found404", metadata.get(Metadata.Key.of("statickey", Metadata.ASCII_STRING_MARSHALLER)));
     }
 }
