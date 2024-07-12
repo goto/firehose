a22d7abcee396b016e0e90d5dd6e4b3e2fdad02a
Eka Winata <eka.winata@gopay.co.id>
2024-07-12 11:29:53 +0700

    Refactor typical cel functionality to util class

--- src/main/java/com/gotocompany/firehose/evaluator/GrpcResponseCelPayloadEvaluator.java
+++ src/main/java/com/gotocompany/firehose/evaluator/GrpcResponseCelPayloadEvaluator.java
@@ -2,20 +2,11 @@ package com.gotocompany.firehose.evaluator;
 
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.Message;
-import com.gotocompany.firehose.exception.DeserializerException;
-import dev.cel.common.CelAbstractSyntaxTree;
-import dev.cel.common.CelValidationException;
-import dev.cel.common.types.StructTypeReference;
+import com.gotocompany.firehose.utils.CelUtils;
 import dev.cel.compiler.CelCompiler;
-import dev.cel.compiler.CelCompilerFactory;
-import dev.cel.parser.CelStandardMacro;
-import dev.cel.runtime.CelEvaluationException;
 import dev.cel.runtime.CelRuntime;
 import dev.cel.runtime.CelRuntimeFactory;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Implementation of PayloadEvaluator that evaluates gRPC responses using CEL (Common Expression Language).
@@ -42,20 +33,13 @@ public class GrpcResponseCelPayloadEvaluator implements PayloadEvaluator<Message
      *
      * @param payload the gRPC message to be evaluated
      * @return true if the payload passes the evaluation, false otherwise
-     * @throws DeserializerException if the evaluation fails
      */
     @Override
     public boolean evaluate(Message payload) {
         if (!descriptor.getFullName().equals(payload.getDescriptorForType().getFullName())) {
             throw new IllegalArgumentException("Payload does not match descriptor");
         }
-        try {
-            Map<String, Object> args = new HashMap<>();
-            args.put(payload.getDescriptorForType().getFullName(), payload);
-            return (boolean) celProgram.eval(args);
-        } catch (CelEvaluationException e) {
-            throw new DeserializerException("Failed to evaluate payload", e);
-        }
+        return (boolean) CelUtils.evaluate(this.celProgram, payload);
     }
 
     /**
@@ -64,23 +48,11 @@ public class GrpcResponseCelPayloadEvaluator implements PayloadEvaluator<Message
      * @param celExpression the CEL expression to evaluate against the message
      * @throws IllegalArgumentException if the CEL expression is invalid or if the evaluator cannot be constructed
      */
-    private void buildCelEnvironment(String celExpression)  {
-        try {
-            CelCompiler celCompiler = CelCompilerFactory.standardCelCompilerBuilder()
-                    .setStandardMacros(CelStandardMacro.values())
-                    .addVar(this.descriptor.getFullName(), StructTypeReference.create(this.descriptor.getFullName()))
-                    .addMessageTypes(this.descriptor)
-                    .build();
-            CelRuntime celRuntime = CelRuntimeFactory.standardCelRuntimeBuilder()
-                    .build();
-            CelAbstractSyntaxTree celAbstractSyntaxTree = celCompiler.compile(celExpression)
-                    .getAst();
-            this.celProgram = celRuntime.createProgram(celAbstractSyntaxTree);
-        } catch (CelValidationException e) {
-            throw new IllegalArgumentException("Invalid CEL expression: " + celExpression, e);
-        } catch (CelEvaluationException e) {
-            throw new IllegalArgumentException("Failed to construct CEL evaluator: " + e.getMessage(), e);
-        }
+    private void buildCelEnvironment(String celExpression) {
+        CelCompiler celCompiler = CelUtils.initializeCelCompiler(this.descriptor);
+        CelRuntime celRuntime = CelRuntimeFactory.standardCelRuntimeBuilder()
+                .build();
+        this.celProgram = CelUtils.initializeCelProgram(celExpression, celRuntime, celCompiler);
     }
 
 }
---
+++ src/main/java/com/gotocompany/firehose/utils/CelUtils.java
@@ -0,0 +1,66 @@
+package com.gotocompany.firehose.utils;
+
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.Message;
+import dev.cel.common.CelAbstractSyntaxTree;
+import dev.cel.common.CelValidationException;
+import dev.cel.common.types.StructTypeReference;
+import dev.cel.compiler.CelCompiler;
+import dev.cel.compiler.CelCompilerFactory;
+import dev.cel.parser.CelStandardMacro;
+import dev.cel.runtime.CelEvaluationException;
+import dev.cel.runtime.CelRuntime;
+import org.aeonbits.owner.util.Collections;
+
+/**
+ * Utility class to instantiate CEL(Common Expression Language) related functionality.
+ * <a href="https://github.com/google/cel-spec">Official Documentation of CEL</a>
+ */
+public class CelUtils {
+
+    /**
+     * @param program the program to execute
+     * @param payload the proto payload to evaluated by program
+     * @return the dynamic value based on program execution of the payload
+     */
+    public static Object evaluate(CelRuntime.Program program, Message payload) {
+        try {
+            return program.eval(Collections.map(payload.getDescriptorForType().getFullName(), payload));
+        } catch (CelEvaluationException e) {
+            throw new IllegalArgumentException("Could not evaluate Cel expression", e);
+        }
+    }
+
+    /**
+     * Initializes the CEL compiler with standard macros and message types.
+     *
+     * @return the initialized CEL compiler
+     */
+    public static CelCompiler initializeCelCompiler(Descriptors.Descriptor descriptor) {
+        return CelCompilerFactory.standardCelCompilerBuilder()
+                .setStandardMacros(CelStandardMacro.values())
+                .addVar(descriptor.getFullName(), StructTypeReference.create(descriptor.getFullName()))
+                .addMessageTypes(descriptor)
+                .build();
+    }
+
+    /**
+     * Initializes a CEL program for a given expression.
+     *
+     * @param celExpression the CEL expression to compile
+     * @param celRuntime    the CEL runtime environment
+     * @param celCompiler   the CEL compiler
+     * @return the compiled CEL program
+     * @throws IllegalArgumentException if the CEL program cannot be created
+     */
+    public static CelRuntime.Program initializeCelProgram(String celExpression, CelRuntime celRuntime, CelCompiler celCompiler) {
+        try {
+            CelAbstractSyntaxTree celAbstractSyntaxTree = celCompiler.compile(celExpression)
+                    .getAst();
+            return celRuntime.createProgram(celAbstractSyntaxTree);
+        } catch (CelValidationException | CelEvaluationException e) {
+            throw new IllegalArgumentException("Failed to create CEL program with expression : " + celExpression, e);
+        }
+    }
+
+}
