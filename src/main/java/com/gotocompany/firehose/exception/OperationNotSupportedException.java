3caa4c07b8dedae89cad3065eb915e512bdecaf5
Eka Winata <eka.winata@gopay.co.id>
2024-07-18 10:18:13 +0700

    Remove OperationNotSupportedException.java

--- src/main/java/com/gotocompany/firehose/exception/OperationNotSupportedException.java
+++
@@ -1,7 +0,0 @@
-package com.gotocompany.firehose.exception;
-
-public class OperationNotSupportedException extends RuntimeException {
-    public OperationNotSupportedException(String message) {
-        super(message);
-    }
-}
