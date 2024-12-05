eebb60751127f48de55adeac87f178be58aa67c2
Sumit Aich <aich.1998@gmail.com>
2024-12-05 16:58:33 +0530

    refactor: rename OSS class to ObjectStorageService

--- src/test/java/com/gotocompany/firehose/config/OSSConfigTest.java
+++ src/test/java/com/gotocompany/firehose/config/OSSConfigTest.java
@@ -12,44 +12,44 @@ public class OSSConfigTest {
     @Test
     public void shouldParseConfigForSink() {
         Map<String, String> properties = new HashMap<String, String>() {{
-            put("SINK_BLOB_OSS_ENDPOINT", "oss-cn-hangzhou.aliyuncs.com");
-            put("SINK_BLOB_OSS_ACCESS_KEY_ID", "test-key-id");
-            put("SINK_BLOB_OSS_ACCESS_KEY_SECRET", "test-key-secret");
+            put("SINK_BLOB_OSS_ENDPOINT", "test-endpoint");
+            put("SINK_BLOB_OSS_ACCESS_KEY_ID", "test-key");
+            put("SINK_BLOB_OSS_ACCESS_KEY_SECRET", "test-secret");
             put("SINK_BLOB_OSS_BUCKET_NAME", "test-bucket");
-            put("SINK_BLOB_OSS_REGION", "cn-hangzhou");
             put("SINK_BLOB_OSS_DIRECTORY_PREFIX", "test-prefix");
+            put("SINK_BLOB_OSS_MAX_CONNECTIONS", "2048");
+            put("SINK_BLOB_OSS_SOCKET_TIMEOUT", "60000");
+            put("SINK_BLOB_OSS_CONNECTION_TIMEOUT", "60000");
+            put("SINK_BLOB_OSS_MAX_ERROR_RETRY", "5");
             put("OSS_TYPE", "SINK_BLOB");
         }};
 
         OSSConfig ossConfig = ConfigFactory.create(OSSConfig.class, properties);
-        Assert.assertEquals("oss-cn-hangzhou.aliyuncs.com", ossConfig.getOSSEndpoint());
-        Assert.assertEquals("test-key-id", ossConfig.getOSSAccessKeyId());
-        Assert.assertEquals("test-key-secret", ossConfig.getOSSAccessKeySecret());
+        Assert.assertEquals("test-endpoint", ossConfig.getOSSEndpoint());
+        Assert.assertEquals("test-key", ossConfig.getOSSAccessKeyId());
+        Assert.assertEquals("test-secret", ossConfig.getOSSAccessKeySecret());
         Assert.assertEquals("test-bucket", ossConfig.getOSSBucketName());
-        Assert.assertEquals("cn-hangzhou", ossConfig.getOSSRegion());
         Assert.assertEquals("test-prefix", ossConfig.getOSSDirectoryPrefix());
+        Assert.assertEquals(Integer.valueOf(2048), ossConfig.getOSSMaxConnections());
+        Assert.assertEquals(Integer.valueOf(60000), ossConfig.getOSSSocketTimeout());
+        Assert.assertEquals(Integer.valueOf(60000), ossConfig.getOSSConnectionTimeout());
+        Assert.assertEquals(Integer.valueOf(5), ossConfig.getOSSMaxErrorRetry());
+    }
+
+    @Test
+    public void shouldUseDefaultValues() {
+        Map<String, String> properties = new HashMap<String, String>() {{
+            put("SINK_BLOB_OSS_ENDPOINT", "test-endpoint");
+            put("SINK_BLOB_OSS_ACCESS_KEY_ID", "test-key");
+            put("SINK_BLOB_OSS_ACCESS_KEY_SECRET", "test-secret");
+            put("SINK_BLOB_OSS_BUCKET_NAME", "test-bucket");
+            put("OSS_TYPE", "SINK_BLOB");
+        }};
+
+        OSSConfig ossConfig = ConfigFactory.create(OSSConfig.class, properties);
         Assert.assertEquals(Integer.valueOf(1024), ossConfig.getOSSMaxConnections());
         Assert.assertEquals(Integer.valueOf(50000), ossConfig.getOSSSocketTimeout());
         Assert.assertEquals(Integer.valueOf(50000), ossConfig.getOSSConnectionTimeout());
         Assert.assertEquals(Integer.valueOf(3), ossConfig.getOSSMaxErrorRetry());
-    }
-
-    @Test
-    public void shouldParseConfigForDLQ() {
-        Map<String, String> properties = new HashMap<String, String>() {{
-            put("DLQ_BLOB_STORAGE_OSS_ENDPOINT", "oss-cn-hangzhou.aliyuncs.com");
-            put("DLQ_BLOB_STORAGE_OSS_ACCESS_KEY_ID", "test-key-id");
-            put("DLQ_BLOB_STORAGE_OSS_ACCESS_KEY_SECRET", "test-key-secret");
-            put("DLQ_BLOB_STORAGE_OSS_BUCKET_NAME", "test-bucket");
-            put("DLQ_BLOB_STORAGE_OSS_REGION", "cn-hangzhou");
-            put("OSS_TYPE", "DLQ_BLOB_STORAGE");
-        }};
-
-        OSSConfig ossConfig = ConfigFactory.create(OSSConfig.class, properties);
-        Assert.assertEquals("oss-cn-hangzhou.aliyuncs.com", ossConfig.getOSSEndpoint());
-        Assert.assertEquals("test-key-id", ossConfig.getOSSAccessKeyId());
-        Assert.assertEquals("test-key-secret", ossConfig.getOSSAccessKeySecret());
-        Assert.assertEquals("test-bucket", ossConfig.getOSSBucketName());
-        Assert.assertEquals("cn-hangzhou", ossConfig.getOSSRegion());
     }
 }
