fa1ca1df35642780704716186fdbe0678a142fde
Sumit Aich <aich.1998@gmail.com>
2024-12-05 16:59:48 +0530

    refactor: rename OSS class to ObjectStorageService

--- src/test/java/com/gotocompany/firehose/sink/common/blobstorage/BlobStorageFactoryTest.java
+++ src/test/java/com/gotocompany/firehose/sink/common/blobstorage/BlobStorageFactoryTest.java
@@ -1,11 +1,10 @@
 package com.gotocompany.firehose.sink.common.blobstorage;
 
 import com.gotocompany.firehose.sink.common.blobstorage.gcs.GoogleCloudStorage;
-import com.gotocompany.firehose.sink.common.blobstorage.oss.OSS;
 import com.gotocompany.firehose.sink.common.blobstorage.s3.S3;
-import org.junit.Rule;
+import com.gotocompany.firehose.sink.common.blobstorage.oss.ObjectStorageService;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,89 +13,62 @@ import static org.junit.Assert.assertTrue;
 
 public class BlobStorageFactoryTest {
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+    private Map<String, String> config;
+
+    @Before
+    public void setup() {
+        config = new HashMap<>();
+    }
 
     @Test
     public void shouldCreateGCSStorage() {
-        Map<String, String> config = new HashMap<String, String>() {{
-            put("GCS_TYPE", "SINK_BLOB");
-            put("SINK_BLOB_GCS_BUCKET_NAME", "test-bucket");
-            put("SINK_BLOB_GCS_CREDENTIAL_PATH", "src/test/resources/gcs-credentials.json");
-            put("SINK_BLOB_GCS_GOOGLE_CLOUD_PROJECT_ID", "test-project");
-        }};
-
+        config.put("GCS_TYPE", "SINK_BLOB");
+        config.put("SINK_BLOB_GCS_BUCKET_NAME", "test-bucket");
+        config.put("SINK_BLOB_GCS_GOOGLE_CLOUD_PROJECT_ID", "test-project");
+        
         BlobStorage storage = BlobStorageFactory.createObjectStorage(BlobStorageType.GCS, config);
         assertTrue(storage instanceof GoogleCloudStorage);
     }
 
     @Test
     public void shouldCreateS3Storage() {
-        Map<String, String> config = new HashMap<String, String>() {{
-            put("S3_TYPE", "SINK_BLOB");
-            put("SINK_BLOB_S3_BUCKET_NAME", "test-bucket");
-            put("SINK_BLOB_S3_REGION", "us-east-1");
-        }};
-
+        config.put("S3_TYPE", "SINK_BLOB");
+        config.put("SINK_BLOB_S3_BUCKET_NAME", "test-bucket");
+        config.put("SINK_BLOB_S3_REGION", "us-east-1");
+        
         BlobStorage storage = BlobStorageFactory.createObjectStorage(BlobStorageType.S3, config);
         assertTrue(storage instanceof S3);
     }
 
     @Test
     public void shouldCreateOSSStorage() {
-        Map<String, String> config = new HashMap<String, String>() {{
-            put("OSS_TYPE", "SINK_BLOB");
-            put("SINK_BLOB_OSS_ENDPOINT", "oss-cn-hangzhou.aliyuncs.com");
-            put("SINK_BLOB_OSS_BUCKET_NAME", "test-bucket");
-            put("SINK_BLOB_OSS_ACCESS_KEY_ID", "test-key");
-            put("SINK_BLOB_OSS_ACCESS_KEY_SECRET", "test-secret");
-        }};
-
+        config.put("OSS_TYPE", "SINK_BLOB");
+        config.put("SINK_BLOB_OSS_ENDPOINT", "test-endpoint");
+        config.put("SINK_BLOB_OSS_ACCESS_KEY_ID", "test-key");
+        config.put("SINK_BLOB_OSS_ACCESS_KEY_SECRET", "test-secret");
+        config.put("SINK_BLOB_OSS_BUCKET_NAME", "test-bucket");
+        
         BlobStorage storage = BlobStorageFactory.createObjectStorage(BlobStorageType.OSS, config);
-        assertTrue(storage instanceof OSS);
+        assertTrue(storage instanceof ObjectStorageService);
     }
 
-    @Test
-    public void shouldThrowExceptionForInvalidGCSConfig() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Exception while creating GCS Storage");
-
-        Map<String, String> config = new HashMap<String, String>() {{
-            put("GCS_TYPE", "SINK_BLOB");
-        }};
-
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForGCSWithInvalidConfig() {
         BlobStorageFactory.createObjectStorage(BlobStorageType.GCS, config);
     }
 
-    @Test
-    public void shouldThrowExceptionForInvalidS3Config() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Exception while creating S3 Storage");
-
-        Map<String, String> config = new HashMap<String, String>() {{
-            put("S3_TYPE", "SINK_BLOB");
-        }};
-
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForS3WithInvalidConfig() {
         BlobStorageFactory.createObjectStorage(BlobStorageType.S3, config);
     }
 
-    @Test
-    public void shouldThrowExceptionForInvalidOSSConfig() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Exception while creating OSS Storage");
-
-        Map<String, String> config = new HashMap<String, String>() {{
-            put("OSS_TYPE", "SINK_BLOB");
-        }};
-
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForOSSWithInvalidConfig() {
         BlobStorageFactory.createObjectStorage(BlobStorageType.OSS, config);
     }
 
-    @Test
-    public void shouldThrowExceptionForUnsupportedStorageType() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Blob Storage Type null is not supported");
-
-        BlobStorageFactory.createObjectStorage(null, new HashMap<>());
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForUnsupportedType() {
+        BlobStorageFactory.createObjectStorage(null, config);
     }
 }
