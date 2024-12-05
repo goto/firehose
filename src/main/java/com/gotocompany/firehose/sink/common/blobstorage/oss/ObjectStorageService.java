000be33a38a32ccf92d04500a55c4f0504ee70d6
Sumit Aich <aich.1998@gmail.com>
2024-12-05 16:55:08 +0530

    refactor: rename OSS class to ObjectStorageService

--- src/main/java/com/gotocompany/firehose/sink/common/blobstorage/oss/ObjectStorageService.java
+++ src/main/java/com/gotocompany/firehose/sink/common/blobstorage/oss/ObjectStorageService.java
@@ -3,12 +3,14 @@ package com.gotocompany.firehose.sink.common.blobstorage.oss;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.OSSException;
-import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.ObjectMetadata;
 import com.gotocompany.firehose.config.OSSConfig;
 import com.gotocompany.firehose.sink.common.blobstorage.BlobStorage;
 import com.gotocompany.firehose.sink.common.blobstorage.BlobStorageException;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.file.Paths;
 
 public class ObjectStorageService implements BlobStorage {
@@ -21,27 +23,35 @@ public class ObjectStorageService implements BlobStorage {
                 .build(config.getOSSEndpoint(),
                         config.getOSSAccessKeyId(),
                         config.getOSSAccessKeySecret());
-        
         this.bucketName = config.getOSSBucketName();
         this.directoryPrefix = config.getOSSDirectoryPrefix();
     }
 
     @Override
-    public void store(String objectName, String localPath) throws BlobStorageException {
+    public void store(String objectName, String localFilePath) throws BlobStorageException {
         try {
-            String fullObjectPath = Paths.get(directoryPrefix, objectName).toString();
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, fullObjectPath, new File(localPath));
-            ossClient.putObject(putObjectRequest);
+            String fullPath = Paths.get(directoryPrefix, objectName).toString();
+            File file = new File(localFilePath);
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.length());
+            
+            try (FileInputStream input = new FileInputStream(file)) {
+                ossClient.putObject(bucketName, fullPath, input, metadata);
+            }
         } catch (OSSException e) {
             throw new BlobStorageException(e.getErrorCode(), "OSS Upload failed", e);
+        } catch (IOException e) {
+            throw new BlobStorageException("IO_ERROR", "Failed to read local file", e);
         }
     }
 
     @Override
     public void store(String objectName, byte[] content) throws BlobStorageException {
         try {
-            String fullObjectPath = Paths.get(directoryPrefix, objectName).toString();
-            ossClient.putObject(bucketName, fullObjectPath, content);
+            String fullPath = Paths.get(directoryPrefix, objectName).toString();
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(content.length);
+            ossClient.putObject(bucketName, fullPath, content);
         } catch (OSSException e) {
             throw new BlobStorageException(e.getErrorCode(), "OSS Upload failed", e);
         }
