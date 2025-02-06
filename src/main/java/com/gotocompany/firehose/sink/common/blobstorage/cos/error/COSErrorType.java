82206f46b222fdb5bc50e1b79e2c36d86663c60d
Sumit Aich <aich.1998@gmail.com>
2025-02-06 10:31:41 +0530

    fix: fix checkstyle errors

--- src/main/java/com/gotocompany/firehose/sink/common/blobstorage/cos/error/COSErrorType.java
+++ src/main/java/com/gotocompany/firehose/sink/common/blobstorage/cos/error/COSErrorType.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public enum COSErrorType {
     BAD_REQUEST(400),
-    UNAUTHORIZED(401), 
+    UNAUTHORIZED(401),
     FORBIDDEN(403),
     NOT_FOUND(404),
     METHOD_NOT_ALLOWED(405),
