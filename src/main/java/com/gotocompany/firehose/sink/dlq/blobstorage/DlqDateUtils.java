17faa6c901db11158a6420e706dd86ad29fc3790
Sumit Aich <aich.1998@gmail.com>
2026-01-23 18:17:18 +0530

    refactor: simplify date extraction logic in DlqDateUtils by consolidating methods

--- src/main/java/com/gotocompany/firehose/sink/dlq/blobstorage/DlqDateUtils.java
+++ src/main/java/com/gotocompany/firehose/sink/dlq/blobstorage/DlqDateUtils.java
@@ -4,29 +4,30 @@ import com.gotocompany.firehose.message.Message;
 import com.gotocompany.firehose.sink.dlq.DlqPartitionKeyType;
 
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class DlqDateUtils {
 
     public static String getDateFromMessage(Message message, ZoneId zoneId) {
-        LocalDate consumeLocalDate = LocalDate.from(Instant.ofEpochMilli(message.getConsumeTimestamp()).atZone(zoneId));
-        return DateTimeFormatter.ISO_LOCAL_DATE.format(consumeLocalDate);
+        return getDateFromMessage(
+                message,
+                zoneId,
+                DlqPartitionKeyType.CONSUME_TIMESTAMP
+        );
     }
 
     public static String getDateFromMessage(Message message, ZoneId zoneId, DlqPartitionKeyType partitionKeyType) {
-        long timestamp;
-        if (partitionKeyType == DlqPartitionKeyType.PRODUCE_TIMESTAMP) {
-            timestamp = message.getTimestamp();
-            if (timestamp <= 0) {
-                timestamp = message.getConsumeTimestamp();
-            }
-        } else {
-            timestamp = message.getConsumeTimestamp();
-        }
-        LocalDate localDate = LocalDate.from(Instant.ofEpochMilli(timestamp).atZone(zoneId));
-        return DateTimeFormatter.ISO_LOCAL_DATE.format(localDate);
+        long timestamp =
+                (partitionKeyType == DlqPartitionKeyType.PRODUCE_TIMESTAMP
+                        && message.getTimestamp() > 0)
+                        ? message.getTimestamp()
+                        : message.getConsumeTimestamp();
+
+        return Instant.ofEpochMilli(timestamp)
+                .atZone(zoneId)
+                .toLocalDate()
+                .format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 }
 
